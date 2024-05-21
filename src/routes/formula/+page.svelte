71fab9fe7d50794031f103e2ca2df29ad70df636
gillilo <ghlwns1535@gmail.com>
2024-05-21 10:56:21 +0900

    1

--- src/routes/formula/+page.svelte
+++ src/routes/formula/+page.svelte
@@ -1,33 +1,70 @@
 <script>
 	const formulas = [
-        {
-            name: "Epley 공식",
-            formula: "1RM = Weight × (1 + 0.0333 × Reps)"
-        },
-        {
-            name: "Brzycki 공식",
-            formula: "1RM = Weight / (1.0278 - 0.0278 × Reps)"
-        },
-        {
-            name: "Lander 공식",
-            formula: "1RM = (Weight × 100) / (101.3 - 2.67123 × Reps)"
-        },
-        {
-            name: "Lombardi 공식",
-            formula: "1RM = Weight × Reps^0.10"
-        },
-        {
-            name: "Mayhew et al. 공식",
-            formula: "1RM = Weight / (0.522 + 0.419 × e^-0.055 × Reps)"
-        },
-        {
-            name: "O'Conner et al. 공식",
-            formula: "1RM = Weight × (1 + 0.025 × Reps)"
-        },
-        {
-            name: "Wathan 공식",
-            formula: "1RM = (Weight × 100) / (48.8 + 53.8 × e^-0.075 × Reps)"
-        }
+		{
+			name: "Epley",
+			abbreviation: "EPL",
+			formula: '(weight, reps) => reps > 1 ? weight * (1 + reps / 30) : weight'
+		},
+		{
+			name: "Brzycki",
+			abbreviation: "BRZ",
+			formula: '(weight, reps) => weight / (1.0278 - 0.0278 * reps)'
+		},
+		{
+			name: "Adams",
+			abbreviation: "ADM",
+			formula: '(weight, reps) => weight / (1 - 0.02 * reps)'
+		},
+		{
+			name: "Baechle",
+			abbreviation: "BCH",
+			formula: '(weight, reps) => weight * (1 + 0.033 * reps)'
+		},
+		{
+			name: "Berger",
+			abbreviation: "BER",
+			formula: '(weight, reps) => weight / (1.0261 * Math.exp(-0.0262 * reps))'
+		},
+		{
+			name: "Brown",
+			abbreviation: "BRN",
+			formula: '(weight, reps) => weight * (0.9849 + 0.0328 * reps)'
+		},
+		{
+			name: "Kemmler et al.",
+			abbreviation: "KML",
+			formula: '(weight, reps) => weight * (0.988 + 0.0104 * reps + 0.00190 * Math.pow(reps, 2) - 0.0000584 * Math.pow(reps, 3))'
+		},
+		{
+			name: "Landers",
+			abbreviation: "LND",
+			formula: '(weight, reps) => weight / (1.013 - 0.0267123 * reps)'
+		},
+		{
+			name: "Lombardi",
+			abbreviation: "LMB",
+			formula: '(weight, reps) => weight * Math.pow(reps, 0.10)'
+		},
+		{
+			name: "Mayhew et al.",
+			abbreviation: "MYH",
+			formula: '(weight, reps) => weight / (0.522 + 0.419 * Math.exp(-0.055 * reps))'
+		},
+		{
+			name: "Naclerio et al.",
+			abbreviation: "NCL",
+			formula: '(weight, reps) => weight / (0.951 * Math.exp(-0.021 * reps))'
+		},
+		{
+			name: "O'Conner et al.",
+			abbreviation: "OCN",
+			formula: '(weight, reps) => weight * (1 + 0.025 * reps)'
+		},
+		{
+			name: "Wathen",
+			abbreviation: "WTH",
+			formula: '(weight, reps) => weight / (0.4880 + 0.538 * Math.exp(-0.075 * reps))'
+		}
     ]
 </script>
 
